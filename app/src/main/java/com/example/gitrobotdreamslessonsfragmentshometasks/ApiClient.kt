1935f191f189bc98df1fb7d882109eb7f619fe6f
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-21 15:24:54 +0200

    added dependencies for hilt and partialy refactored code with di

---
+++ app/src/main/java/com/example/gitrobotdreamslessonsfragmentshometasks/ApiClient.kt
@@ -0,0 +1,14 @@
+package com.example.gitrobotdreamslessonsfragmentshometasks
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ApiClient @Inject constructor() {
+    val client: Retrofit = Retrofit.Builder()
+        .baseUrl(/* baseUrl = */ "https://akabab.github.io")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
